c5aa6d97cb2f531c5f57a0a263e19f96fca853f9
Steve Seguin <steve@studio3d.tv>
2017-04-17 17:03:12 -0400

    conf file added

--- deploy.sh
+++ deploy.sh
@@ -33,3 +33,52 @@ sudo chmod +x /etc/init.d/nginx
 ## Add nginx to the system startup
 sudo /usr/sbin/update-rc.d -f nginx defaults
 sudo service nginx start
+
+######################
+## sudo service nginx stop 
+## sudo service nginx start 
+## sudo service nginx restart
+## sudo service nginx reload
+################################
+
+## Let's configure it for RTMP ingest using Steve Seguin's example configuration.
+cd /usr/local/openresty/nginx/conf
+sudo mv nginx.conf nginx.conf.bak
+sudo mv ~/nginx/nginx.conf ./nginx.conf
+
+### You can view server stats with the following additional @ http://thisserver.com/stat
+sudo mv ~/nginx/nginx.conf ../stat.xsl
+sudo service nginx restart
+
+## Time to install SSL (optional)
+### Below is how you can install a FREE SSL cert, which lasts 90 days. It requires a domain name.
+# cd ~
+# wget https://dl.eff.org/certbot-auto
+# chmod a+x certbot-auto
+# ./certbot-auto ##### You will need to press "Y" and then Enter during the script 
+# sudo service nginx stop 
+# sudo ./certbot-auto certonly --standalone -d rtmp.stageten.tv ## << substitute current hostname name here
+# sudo service nginx start 
+#### Update nginx's conf file as needed, then restart. I don't see a point in adding SSL though at this time, but it's there.
+#######
+
+### Install FFMPEG ; Optional, but highly recommended if you wish to do advanced features with NGINX
+sudo apt-add-repository ppa:mc3man/trusty-media -y
+sudo apt-get update
+sudo apt-get install ffmpeg -y
+## This version of FFMPEG should support Nvidia GPU; x264, x265, OPUS, mp3, aac, etc.
+##
+
+ffmpeg -version 
+##
+echo 'External IP:' $(wget http://ipinfo.io/ip -qO -)
+######
+#### PERFORMING A 10 SECOND TEST #
+
+timeout --kill-after=10 9 ffmpeg -re -f lavfi -i testsrc="size=640x360:rate=30" -f lavfi -i sine=f=220:b=1 -af volume=0.1 -codec:a aac -strict -2 -codec:v libx264 -preset ultrafast -tune zerolatency -pixel_format yuv420p -g 48 -f flv rtmp://$(wget http://ipinfo.io/ip -qO -)/test/streamname
+
+###############
+## And now just configure the NGINX conf file to suit your needs
+## cd /usr/local/nginx/conf
+## ls -l
+## vi nginx.conf
